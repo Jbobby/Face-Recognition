6f23386038f9fbdcf79d8208f8b4b377fc627ddb
Jbobby <dolapojohnson99@gmail.com>
2019-08-15 12:37:14 +0100

    Components on FaceRecog

---
+++ src/components/Navigation/ImageLinkForm.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ImageLinkForm.css';
+
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
+    return (
+        <div>
+            <h4 className='f3 tc'> This App Recognises The Face When You Upload An Image. Try It Out. </h4>
+            <div className='center'>
+                <div className='shadow-5 br3 pa4 bg-hot-pink pattern'>
+                    <input className='pa2 f4 w-70' type='text' placeholder='Enter Image URL' onChange={ onInputChange }/>
+                    <button className='f4 grow w-30 link pv2 dib white bg-light-purple' onClick={ onButtonSubmit }> Detect </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default ImageLinkForm;
---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navigation = () => {
+    return (
+        <div style={{display: 'flex', justifyContent: 'flex-end'}}>
+            <p className='dim underline f3 pa3 link black pointer'>Sign Out</p>
+        </div>
+    );
+}
+
+export default Navigation;
